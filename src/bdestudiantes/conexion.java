688f9f25c4fca598ac4c9e7eee379a83171af3cd
Lramirezpsj <2023-053063@intecap.edu.gt>
2023-09-28 22:04:23 -0600

    first commit

--- src/bdestudiantes/conexion.java
+++ src/bdestudiantes/conexion.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bdestudiantes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author ramir
- */
+
 public class conexion {
     Connection con;
     String url="jdbc:mysql://localhost:3306/estudiantes";
--- src/bdestudiantes/menu.java
+++ src/bdestudiantes/menu.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bdestudiantes;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ramir
- */
+
 public class menu {
 
     public static void ejecucion() {
